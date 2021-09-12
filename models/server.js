b25dcf6e49b1af02cf83b34e8e4293bb0b072dd3
DarkAstaroth <mninahuanca4@gmail.com>
2021-09-12 19:08:11 -0400

    Conectando la base de datos con mongoose

--- models/server.js
+++ models/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        
+        // Conectar a la base de datos
+        this.conectarDB();
+
+        // Middlewares
+        this.middleware();
+
+        // Rutas de mi aplicacion
+        this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     middleware() {
