aa91b1714c05290ac8a1dc01862896bf51f3dcb6
DarkAstaroth <mninahuanca4@gmail.com>
2021-09-21 23:23:57 -0400

    Haciendo el login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,43 @@
 const { response, request } = require("express");
+const Usuario = require("../models/usuario");
+const bcryptjs = require("bcryptjs");
 
-const login = (req = request, res = response) => {
-  res.json({
-    msg: "Login Ok",
-  });
+const login = async (req = request, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // verificar si el Email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
+    }
+
+    // Si el usuario esta activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - estado:false",
+      });
+    }
+
+    // Verificar la contraseña
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - password",
+      });
+    }
+
+    // Genera el JWT
+    res.json({
+      msg: "Login Ok",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 module.exports = {
