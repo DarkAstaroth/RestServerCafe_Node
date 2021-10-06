841360571f131b9b058910e3f54c39919faf92a3
DarkAstaroth <mninahuanca4@gmail.com>
2021-10-06 18:41:10 -0400

    Realizando busquedas en la base de datos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,12 +1,43 @@
 const { request, response } = require("express");
+const { ObjectId } = require("mongoose").Types;
+const { Usuario, Categoria, Producto } = require("../models");
+
+const coleccionesPermitidas = ["usuarios", "categorias", "productos", "roles"];
+
+const buscarUsuarios = async (termino = "", res = response) => {
+  const esMongoId = ObjectId.isValid(termino);
+  if (esMongoId) {
+    const usuario = await Usuario.findById(termino);
+    return res.json({
+      results: usuario ? [usuario] : [],
+    });
+  }
+};
 
 const buscar = (req = request, res = response) => {
   const { coleccion, termino } = req.params;
 
-  res.json({
-    msg: "Buscando ",
-    coleccion,termino   
-  });
+  if (!coleccionesPermitidas.includes(coleccion)) {
+    return res.status(400).json({
+      msg: `Las colecciones permitidas son ${coleccionesPermitidas}`,
+    });
+  }
+
+  switch (coleccion) {
+    case "usuarios":
+      buscarUsuarios(termino, res);
+      break;
+    case "categorias":
+      break;
+    case "productos":
+      break;
+    default:
+      res.status(500).json({
+        msg: "se le olvido hacer esta busqueda",
+      });
+      break;
+  }
+
 };
 
 module.exports = {
