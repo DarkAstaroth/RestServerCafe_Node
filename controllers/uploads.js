f5b20e092a38ad31a1031d66904bdab1c8719bfb
Manuel <mninahuanca4@gmail.com>
2021-10-09 19:14:00 -0400

    Crear carpeta de destino para los archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -6,11 +6,17 @@ const cargarArchivo = async (req = request, res = response) => {
     res.status(400).json("No hay archivos que subir");
     return;
   }
-  //Imagenes
-  const nombre = await subirArchivo(req.files);
-  res.json({
-    nombre,
-  });
+
+  try {
+    const nombre = await subirArchivo(req.files,undefined,'img');
+    res.json({
+      nombre,
+    });
+  } catch (error) {
+      res.status(400).json({
+          error
+      });
+  }
 };
 
 module.exports = {
