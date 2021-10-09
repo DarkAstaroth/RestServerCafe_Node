f1f63a60cd4598b70e73164dea8fa22f21f385e3
Manuel <mninahuanca4@gmail.com>
2021-10-09 17:18:16 -0400

    validar la extension para subir archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -8,16 +8,28 @@ const cargarArchivo = (req = request, res = response) => {
   }
 
   const { archivo } = req.files;
+  const nombreCortado = archivo.name.split(".");
+  const extension = nombreCortado[nombreCortado.length - 1];
 
-  const uploadPath = path.join(__dirname, "../uploads", archivo.name);
+  // validar la extension
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+  if (!extensionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `La extension ${extension} no es permitida, ${extensionesValidas}`,
+    });
+  }
 
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
+  res.json({ extension });
 
-    res.json({ msg: "File uploaded to " + uploadPath });
-  });
+  //   const uploadPath = path.join(__dirname, "../uploads", archivo.name);
+
+  //   archivo.mv(uploadPath, (err) => {
+  //     if (err) {
+  //       return res.status(500).json({ err });
+  //     }
+
+  //     res.json({ msg: "File uploaded to " + uploadPath });
+  //   });
 };
 
 module.exports = {
