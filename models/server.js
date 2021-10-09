cf8007a4caf42ec552a688b3d35e9ef848c6aca8
Manuel <mninahuanca4@gmail.com>
2021-10-09 16:45:02 -0400

    Subir archivos a nuestro servidor

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("../database/config");
+const fileUpload = require("express-fileupload");
 
 class Server {
   constructor() {
@@ -39,6 +40,14 @@ class Server {
 
     // Directorio publico
     this.app.use(express.static("public"));
+
+    // Manejar el FileUploads - carga de archivos
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+      })
+    );
   }
 
   routes() {
