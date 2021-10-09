baf98fcffe071484bb79d46922055e7d5ea877ab
Manuel <mninahuanca4@gmail.com>
2021-10-09 18:22:04 -0400

    Creando un helper para la funcion de subir archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,36 +1,15 @@
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
-
 const { response, request } = require("express");
+const { subirArchivo } = require("../helpers");
 
-const cargarArchivo = (req = request, res = response) => {
+const cargarArchivo = async (req = request, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json("No hay archivos que subir");
     return;
   }
-
-  const { archivo } = req.files;
-  const nombreCortado = archivo.name.split(".");
-  const extension = nombreCortado[nombreCortado.length - 1];
-
-  // validar la extension
-  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: `La extension ${extension} no es permitida, ${extensionesValidas}`,
-    });
-  }
-
-  const nombreTemporal = uuidv4() + "." + extension;
-
-  const uploadPath = path.join(__dirname, "../uploads", nombreTemporal);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-
-    res.json({ msg: "File uploaded to " + uploadPath });
+  //Imagenes
+  const nombre = await subirArchivo(req.files);
+  res.json({
+    nombre,
   });
 };
 
