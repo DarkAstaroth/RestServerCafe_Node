aabc79e770546e3c7a1370bf955f8e2cf6525915
DarkAstaroth <mninahuanca4@gmail.com>
2021-10-11 16:18:03 -0400

    validando subida de archivos undefined

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,15 +2,17 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const { cargarArchivo, actualizarImagen } = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
+const { validarArchivoSubir } = require("../middlewares/validar-archivo");
 const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
-router.post("/", cargarArchivo);
+router.post("/", validarArchivoSubir, cargarArchivo);
 
 router.put(
   "/:coleccion/:id",
   [
+    validarArchivoSubir,
     check("id", "EL id debe de ser de mongo").isMongoId(),
     check("coleccion").custom((c) =>
       coleccionesPermitidas(c, ["usuarios", "productos"])
